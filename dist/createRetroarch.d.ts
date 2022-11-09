49e3fa637f99dfadd0e962353cf7e4b666a0f2c9
dimitrikarpov <karpov.dimon@gmail.com>
2022-11-09 10:05:53 +0500

    forgot to build package... repacking

--- dist/createRetroarch.d.ts
+++ dist/createRetroarch.d.ts
@@ -4,6 +4,7 @@ export declare type TCreateRetroarchOptions = {
     core: TCore;
     rom?: Uint8Array;
     save?: Uint8Array;
+    canvas?: HTMLCanvasElement;
     container?: HTMLDivElement;
 };
-export declare const createRetroarch: ({ core, rom, save, container, }: TCreateRetroarchOptions) => Promise<Retroarch>;
+export declare const createRetroarch: ({ core, rom, save, canvas, container, }: TCreateRetroarchOptions) => Promise<Retroarch>;
